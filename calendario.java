f6cf6f1d1731b00191491006bdf76e7ab191f8dd
Nane <bruna.negrelli@ufpr.br>
2022-03-21 20:14:10 -0300

    [update] main functions added

--- calendario.java
+++ calendario.java
@@ -1,50 +1,98 @@
 import java.util.Scanner;
 
 public class Calendario {
+    
+    public static String mostrarMesIf(int mes) {
+        String nomeMes = "";
+
+        if (mes == 1) {
+           nomeMes = "Janeiro";
+        } else if (mes == 2) {
+           nomeMes = "Fevereiro";
+        } else if (mes == 3) {
+           nomeMes = "Março";
+        } else if (mes == 4) {
+            nomeMes = "Abril";
+        } else if (mes == 5) {
+            nomeMes = "Maio";
+        } else if (mes == 6) {
+            nomeMes = "Junho";
+        } else if (mes == 7) {
+            nomeMes = "Julho";
+        } else if (mes == 8) {
+            nomeMes = "Agosto";
+        } else if (mes == 9) {
+            nomeMes = "Setembro";
+        } else if (mes == 10) {
+            nomeMes = "Outubro";
+        } else if (mes == 11) {
+            nomeMes = "Novembro";
+        } else if (mes == 12) {
+            nomeMes = "Dezembro";
+        } else {
+            nomeMes = "Valor inserido inválido!";
+        }
+
+        return nomeMes;
+    }
+
+    public static String mostrarMesSwitch(int mes) {
+        String nomeMes = "";
+
+        switch (mes) {
+            case 1:
+                nomeMes = "Janeiro";
+                break;
+            case 2:
+                nomeMes = "Fevereiro";
+                break;
+            case 3:
+                nomeMes = "Março";
+                break;
+            case 4:
+                nomeMes = "Abril";
+                break;
+            case 5:
+                nomeMes = "Maio";
+                break;
+            case 6:
+                nomeMes = "Junho";
+                break;
+            case 7:
+                nomeMes = "Julho";
+                break;
+            case 8:
+                nomeMes = "Agosto";
+                break;
+            case 9:
+                nomeMes = "Setembro";
+                break;
+            case 10:
+                nomeMes = "Outubro";
+                break;
+            case 11:
+                nomeMes = "Novembro";
+                break;
+            case 12:
+                nomeMes = "Dezembro";
+                break;
+            default:
+                nomeMes = "Valor inserido inválido!";
+        }
+        return nomeMes;
+    }
     public static void main(String[] arg) {
         int mes;
-        mes = 7;
+        mes = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("Digite o mês: ");
         mes = input.nextInt();
 
-        if (mes == 1) {
-            System.out.println("Janeiro");
-        }
-        if (mes == 2) {
-            System.out.println("Fevereiro");
-        }
-        if (mes == 3) {
-            System.out.println("Março");
-        }
-        if (mes == 4) {
-            System.out.println("Abril");
-        }
-        if (mes == 5) {
-            System.out.println("Maio");
-        }
-        if (mes == 6) {
-            System.out.println("Junho");
-        }
-        if (mes == 7) {
-            System.out.println("Julho");
-        }
-        if (mes == 8) {
-            System.out.println("Agosto");
-        }
-        if (mes == 9) {
-            System.out.println("Setembro");
-        }
-        if (mes == 10) {
-            System.out.println("Outubro");
-        }
-        if (mes == 11) {
-            System.out.println("Novembro");
-        }
-        if (mes == 12) {
-            System.out.println("Dezembro");
-        }
+        System.out.println("Mes (if) = " + mostrarMesIf(mes));
+        System.out.println("Mes (switch) = " + mostrarMesSwitch(mes));
 
         input.close();
     }
+
+   
 }
